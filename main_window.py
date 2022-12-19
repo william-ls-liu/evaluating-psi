c3c5a2e7bf605a4923e382fe31d667626289a417
William Liu <will123liu@gmail.com>
2022-12-19 14:52:50 -0800

    Create a MainWindow and some buttons with icons.

--- main_window.py
+++ main_window.py
@@ -1,11 +1,51 @@
 # Author: William Liu <liwi@ohsu.edu>
 
-from PySide6 import QtCore, QtWidgets
-import pyqtgraph as pg
-from USB6210 import DAQ
-import numpy as np
-from sounds import Countdown
-from progress_widget import ProgressWidget
-from data_viewer import GraphWindow
-import csv
-from datetime import datetime
+from PySide6 import QtCore, QtWidgets, QtGui
+
+
+class MainWindow(QtWidgets.QMainWindow):
+    """This class represents the main window of the GUI."""
+    def __init__(self) -> None:
+        super().__init__(parent=None)
+
+        # Set the title
+        self.setWindowTitle("PSI Data Collection Software")
+
+        # A MainWindow needs a central widget to serve as a container for all other widgets
+        self.central_widget = QtWidgets.QWidget(parent=self)
+
+        # Add a toolbar
+        self.toolbar = ControlBar(self)
+
+        # Create a layout
+        layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(self.toolbar)
+        layout.setAlignment(QtCore.Qt.AlignmentFlag.AlignTop)
+
+        self.central_widget.setLayout(layout)
+        self.setCentralWidget(self.central_widget)
+
+
+class ControlBar(QtWidgets.QWidget):
+    """This is the toolbar that holds the buttons that control the software."""
+    def __init__(self, parent) -> None:
+        super().__init__(parent=parent)
+        self.stop_button = QtWidgets.QPushButton(parent=self, icon=QtGui.QIcon("icons/16/142.png"))
+        # self.stop_button.setFixedSize(20, 20)
+        self.start_button = QtWidgets.QPushButton(parent=self, icon=QtGui.QIcon("icons/16/131.png"))
+        # self.start_button.setFixedSize(20, 20)
+        self.record_button = QtWidgets.QPushButton(parent=self, icon=QtGui.QIcon("icons/16/151.png"))
+
+        layout = QtWidgets.QHBoxLayout()
+        layout.addWidget(self.stop_button)
+        layout.addWidget(self.start_button)
+        layout.addWidget(self.record_button)
+        # layout.addStretch(1)
+
+        self.setLayout(layout)
+
+
+app = QtWidgets.QApplication()
+window = MainWindow()
+window.showMaximized()
+app.exec()
