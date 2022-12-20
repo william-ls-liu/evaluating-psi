d6f9e79f740ad6c5292b3f124cc0afb0a3cb01f5
William Liu <will123liu@gmail.com>
2022-12-20 09:15:01 -0800

    Optimize import statements.

--- main_window.py
+++ main_window.py
@@ -1,9 +1,12 @@
 # Author: William Liu <liwi@ohsu.edu>
 
-from PySide6 import QtCore, QtWidgets, QtGui
+from PySide6.QtCore import Qt
+from PySide6.QtGui import QIcon
+from PySide6.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QApplication
+from plot_widget import PlotWidget
 
 
-class MainWindow(QtWidgets.QMainWindow):
+class MainWindow(QMainWindow):
     """This class represents the main window of the GUI."""
     def __init__(self) -> None:
         super().__init__(parent=None)
@@ -12,31 +15,35 @@ class MainWindow(QtWidgets.QMainWindow):
         self.setWindowTitle("PSI Data Collection Software")
 
         # A MainWindow needs a central widget to serve as a container for all other widgets
-        self.central_widget = QtWidgets.QWidget(parent=self)
+        self.central_widget = QWidget(parent=self)
 
         # Add a toolbar
-        self.toolbar = ControlBar(self)
+        self.control_bar = ControlBar(self)
+
+        # Add plot widget
+        self.plot_widget = PlotWidget(self)
 
         # Create a layout
-        layout = QtWidgets.QVBoxLayout()
-        layout.addWidget(self.toolbar)
-        layout.setAlignment(QtCore.Qt.AlignmentFlag.AlignTop)
+        layout = QVBoxLayout()
+        layout.addWidget(self.control_bar)
+        layout.addWidget(self.plot_widget)
+        layout.setAlignment(Qt.AlignmentFlag.AlignTop)
 
         self.central_widget.setLayout(layout)
         self.setCentralWidget(self.central_widget)
 
 
-class ControlBar(QtWidgets.QWidget):
+class ControlBar(QWidget):
     """This is the toolbar that holds the buttons that control the software."""
     def __init__(self, parent) -> None:
         super().__init__(parent=parent)
-        self.stop_button = QtWidgets.QPushButton(parent=self, icon=QtGui.QIcon("icons/16/142.png"))
+        self.stop_button = QPushButton(parent=self, icon=QIcon("icons/16/142.png"))
         # self.stop_button.setFixedSize(20, 20)
-        self.start_button = QtWidgets.QPushButton(parent=self, icon=QtGui.QIcon("icons/16/131.png"))
+        self.start_button = QPushButton(parent=self, icon=QIcon("icons/16/131.png"))
         # self.start_button.setFixedSize(20, 20)
-        self.record_button = QtWidgets.QPushButton(parent=self, icon=QtGui.QIcon("icons/16/151.png"))
+        self.record_button = QPushButton(parent=self, icon=QIcon("icons/16/151.png"))
 
-        layout = QtWidgets.QHBoxLayout()
+        layout = QHBoxLayout()
         layout.addWidget(self.stop_button)
         layout.addWidget(self.start_button)
         layout.addWidget(self.record_button)
@@ -45,7 +52,7 @@ class ControlBar(QtWidgets.QWidget):
         self.setLayout(layout)
 
 
-app = QtWidgets.QApplication()
+app = QApplication()
 window = MainWindow()
 window.showMaximized()
 app.exec()
