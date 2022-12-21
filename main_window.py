47476f7169c8b14d9c0a85dda52f5eeb6af2dafb
William Liu <will123liu@gmail.com>
2022-12-21 11:25:39 -0800

    Connect ControlBar buttons to slots on the DataWorker.
    Also override the closeEvent method to ensure the worker thread is
    terminated and the DAQ processes are closed before the MainWindow exits.

--- main_window.py
+++ main_window.py
@@ -1,6 +1,6 @@
 # Author: William Liu <liwi@ohsu.edu>
 
-from PySide6.QtCore import Qt, QThread, Signal
+from PySide6.QtCore import Qt, QThread, Signal, Slot
 from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QApplication
 from plot_widget import PlotWidget
@@ -29,11 +29,12 @@ class MainWindow(QMainWindow):
         self.data_worker_thread = QThread()
         self.data_worker.moveToThread(self.data_worker_thread)
         self.data_worker_thread.start()
+        self.data_worker_ready_for_shutdown = False  # Important for handling termination of the worker thread
 
         # Connect the signals between the control bar and the worker
         self.control_bar.start_button_signal.connect(self.data_worker.start_sampling)
         self.control_bar.stop_button_signal.connect(self.data_worker.stop_sampling)
-        self.data_worker.data_signal.connect(self.display_data)
+        self.data_worker.data_signal.connect(self.process_incoming_data)
 
         # Create a layout
         layout = QVBoxLayout()
@@ -46,19 +47,21 @@ class MainWindow(QMainWindow):
 
     def closeEvent(self, event):
         """Override of the default close method. Ensure the thread is shut down and the DAQ task is stopped."""
-        # Need to implement a method to shutdown the active DAQ task if one exists. This method will be in the
-        # data worker class.
+        self.data_worker.shutdown()
         self.data_worker_thread.exit()
+        event.accept()
 
-    def display_data(self, data):
+    @Slot(list)
+    def process_incoming_data(self, data):
+        """Slot to receive the data from the DataWorker and process it for use."""
         print(data)
 
 
 class ControlBar(QWidget):
     """This is the toolbar that holds the buttons that control the software."""
-    stop_button_signal = Signal(bool)
-    start_button_signal = Signal(bool)
-    record_button_signal = Signal(bool)
+    stop_button_signal = Signal()
+    start_button_signal = Signal()
+    record_button_signal = Signal()
 
     def __init__(self, parent) -> None:
         super().__init__(parent=parent)
@@ -78,16 +81,15 @@ class ControlBar(QWidget):
 
     def stop_button_clicked(self):
         """Emit the stop signal when the button is clicked."""
-        self.stop_button_signal.emit(True)
+        self.stop_button_signal.emit()
 
     def start_button_clicked(self):
         """Emit the start signal when the button is clicked."""
-        print("start clicked")
-        self.start_button_signal.emit(True)
+        self.start_button_signal.emit()
 
     def record_button_clicked(self):
         """Emit the record signal when the button is clicked."""
-        self.record_button_signal.emit(True)
+        self.record_button_signal.emit()
 
 
 app = QApplication()
