7b49d3d2d6cd54ead6b1da60aae9f0086cb7fed6
William Liu <will123liu@gmail.com>
2022-12-30 11:50:49 -0800

    docs: update docstrings

--- plot_widget.py
+++ plot_widget.py
@@ -7,8 +7,47 @@ import numpy as np
 
 
 class PlotWidget(QWidget):
-    """This widget reads data from the DAQ and holds the graphs to display that data."""
-    def __init__(self, parent) -> None:
+    """
+    Custom widget that receives data and plots it using pyqtgraph.
+
+    Attributes
+    ----------
+    plots : GraphicsLayoutWidget
+        a pyqtgraph GraphicsLayoutWidget that contains subplots
+    timer : PySide6.QtCore.QTimer
+        timer object, on timeout it updates the graphs with any new data
+    copx : list
+        stores the mediolateral (X-direction) center of pressure coordinates
+    copy : list
+        stores the anterioposterior (Y-direction) center of pressure coordinates
+    fz : list
+        stores the vertical (Z-direction) force values
+    emg_tibialis : list
+        stores the voltage values from the EMG placed on the tibialis
+    emg_soleus : list
+        stores the voltage values from the EMG places on the soleus
+
+    Methods
+    -------
+    process_data_from_worker(data: np.ndarray)
+        A Slot. Receives an array of data and stores values in appropriate lists
+    update_plots()
+        A Slot. Called by the timeout of timer. Calls Plots.update to add new data to graphs.
+    start_timer()
+        A Slot that starts timer. QTimers cannot be started/stopped from other threads, but using a signal and slot
+        is threadsafe.
+    stop_timer()
+        A Slot that stops timer.
+    """
+
+    def __init__(self, parent=None) -> None:
+        """
+        Parameters
+        ----------
+        parent : PySide6.QtWidgets.QWidget, optional
+            the parent widget, if None the widget becomes a window
+        """
+
         super().__init__(parent=parent)
 
         # Initiate the pyqygraph widget
@@ -32,8 +71,19 @@ class PlotWidget(QWidget):
         self.setLayout(layout)
 
     @Slot(np.ndarray)
-    def process_data_from_worker(self, data):
-        """Slot to receive the data from the DataWorker and process it for use."""
+    def process_data_from_worker(self, data: np.ndarray) -> None:
+        """
+        Slot to receive data and store it in appropriate lists.
+
+        Incoming data is array-like with values [Fx, Fy, Fz, Mx, My, Mz, EMG Tibialis, EMG Soleus, CoP X, CoP Y].
+        Extract individual components of incoming data and add them to their list.
+
+        Parameters
+        ----------
+        data : np.ndarray
+            incoming data
+        """
+
         self.copx = self.copx[1:]
         self.copx.append(data[8])
         self.copy = self.copy[1:]
@@ -48,24 +98,35 @@ class PlotWidget(QWidget):
         self.emg_soleus.append(data[7])
 
     @Slot()
-    def update_plots(self):
+    def update_plots(self) -> None:
         """Update the graphs with new data."""
+
         self.plots.update(self.copx, self.copy, self.fz, self.emg_tibialis, self.emg_soleus)
 
     @Slot()
-    def start_timer(self):
+    def start_timer(self) -> None:
         """Start the QTimer."""
+
         self.timer.start()
 
     @Slot()
-    def stop_timer(self):
+    def stop_timer(self) -> None:
         """Stop the QTimer."""
+
         self.timer.stop()
 
 
 class Plots(GraphicsLayoutWidget):
-    """This widget represents the different graphs."""
-    def __init__(self, parent):
+    """A class to display a multi-panel pyqtgraph figure."""
+
+    def __init__(self, parent) -> None:
+        """
+        Parameters
+        ----------
+        parent : PySide6.QtWidgets.QWidget
+            the parent widget
+        """
+
         super().__init__(parent=parent)
 
         # Create the center of pressure graph
@@ -94,8 +155,24 @@ class Plots(GraphicsLayoutWidget):
         self.emg_soleus_plot_item.hideAxis('bottom')
         self.emg_soleus_plot_line = self.emg_soleus_plot_item.plot(x=[0], y=[0])
 
-    def update(self, copx, copy, fz, emg_tibialis, emg_soleus):
-        """Update the graphs."""
+    def update(self, copx, copy, fz, emg_tibialis, emg_soleus) -> None:
+        """
+        Update the graphs with new data.
+
+        Parameters
+        ----------
+        copx : list
+            mediolateral center of pressure data
+        copy : list
+            anterioposterior center of pressure data
+        fz : list
+            vertical force data
+        emg_tibialis : list
+            emg data from tibialis sensor
+        emg_soleus : list
+            emg data from soleus sensor
+        """
+
         self.cop_plot_line.setData(x=[copx[-1]], y=[copy[-1]])
         self.fz_plot_line.setData(y=fz)
         self.emg_tibialis_plot_line.setData(y=emg_tibialis)
