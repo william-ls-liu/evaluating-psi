b309aa4026f92a311d9de13d5a207fcd9bb15760
William Liu <will123liu@gmail.com>
2023-01-03 14:43:41 -0800

    docs: add/update docstrings

--- protocol_widget.py
+++ protocol_widget.py
@@ -8,14 +8,26 @@ import numpy as np
 
 
 class ProtocolWidget(QWidget):
-    """This widget holds methods related to PSI protocol."""
+    """This widget provides buttons to control the data collection and labels to show the progress.
+
+    Attributes
+    ----------
+    start_baseline_signal : PySide6.QtCore.Signal
+        a signal that is emitted when `start_baseline_button` is clicked
+    cancel_baseline_signal : PySide6.QtCore.Signal
+        a signal that is emitted when `cancel_baseline_button` is clicked
+    collect_baseline_signal : PySide6.QtCore.Signal
+        a signal that is emitted when `collect_baseline_button` is clicked
+    finish_baseline_signal : PySide6.QtCore.Signal
+        a signal that is emitted when  `finish_baseline_button` is clicked
+    """
 
     start_baseline_signal = Signal()
     cancel_baseline_signal = Signal()
     collect_baseline_signal = Signal()
     finish_baseline_signal = Signal()
 
-    def __init__(self, parent) -> None:
+    def __init__(self, parent: QWidget) -> None:
         super().__init__(parent=parent)
 
         # Create a heading for the Widget
@@ -92,6 +104,12 @@ class ProtocolWidget(QWidget):
 
     @Slot()
     def cancel_baseline_button_clicked(self):
+        """Cancels a baseline recording if user accepts.
+
+        Opens a pop-up message box allowing user to confirm the cancellation or continue
+        with the current recording.
+        """
+
         message_box = QMessageBox.warning(
             self,
             "Warning!",
@@ -141,20 +159,34 @@ class ProtocolWidget(QWidget):
         self.show_baseline_graph()
 
     def show_baseline_graph(self):
-        """This will be called after the finish baseline button is clicked. It will show you the graph of the lateral
-        CoP and give you the option to save it and collect the next trial or discard it and repeat."""
+        """Display the lateral CoP data from the most recent trial.
+
+        Open a dialog window with a graph of the lateral CoP position vs. time. User can choose to save the trial or
+        discard it, depending on how the graph looks.
+        """
+        
         copx = self.calculate_APA(self.temporary_data_storage)
         graph_dialog = GraphDialog(data=copx, parent=self)
         graph_dialog.open()
         graph_dialog.finished.connect(self.handle_baseline_trial)
 
     def calculate_APA(self, data):
-        """
-        Calculate the relative motion of the lateral CoP and return it.
+        """Calculate the relative motion of the lateral CoP and return it.
 
-        :param data: array-like of raw data from the DataWorker
-        :return: array-like of relative lateral deviation of the CoP
+        The relative motion is the distance the lateral CoP has moved from the starting position. Starting position is
+        defined as the mean CoP during 5 seconds of quiet stance.
+
+        Parameters
+        ----------
+        data : list of np.ndarray
+            data from the most recent baseline recording
+
+        Returns
+        -------
+        list
+            a time-series of the relative lateral CoP deviations
         """
+
         # Get the lateral CoP data
         copx = [row[8] for row in data]
         # TODO: get rid of this 5000 magic number, read the settings file and get sample rate, then multiply by 5
@@ -163,12 +195,14 @@ class ProtocolWidget(QWidget):
 
         return [row[8] - x_origin for row in data]
 
-    @Slot()
+    @Slot(int)
     def handle_baseline_trial(self, result):
-        """
-        This method will save or discard the most recent baseline trial, depending on what the user selects.
+        """This method will save or discard the most recent baseline trial, depending on what the user selects.
 
-        :param result: The result code of the GraphDialog. 1 = Save trial, 0 = Repeat trial
+        Parameters
+        ----------
+        result : int
+            result code emitted when `GraphDialog` window is closed, 1 indicates user wants to save the trial
         """
         if result == 1:
             number_of_baseline_trials = len(self.baseline_data)
@@ -200,8 +234,10 @@ class ProtocolWidget(QWidget):
 
     @Slot()
     def set_APA_threshold(self):
-        """
-        This method will calculate the APA threshold based on the collected baseline trials.
+        """This method will calculate the APA threshold based on the collected baseline trials.
+
+        For every trial find the maximum, relative, lateral deviation of the CoP. Find the mean
+        across all trials to get the average lateral CoP deviation during a step.
         """
 
         maximum_lateral_deviation = list()
