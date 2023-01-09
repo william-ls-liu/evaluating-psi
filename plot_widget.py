4359a15dbc054f458653efb63a34da061b60b25a
William Liu <will123liu@gmail.com>
2023-01-09 09:12:05 -0800

    feat: calculate samples to show from sample rate
    Read settings file and get sample rate, then multiply by SECONDS_TO_SHOW
    to get how many samples should be on screen at a time.

--- plot_widget.py
+++ plot_widget.py
@@ -5,6 +5,7 @@ from PySide6.QtCore import Slot, QTimer
 from pyqtgraph import GraphicsLayoutWidget
 import numpy as np
 from collections import namedtuple
+import json
 
 
 AmplifierRange = namedtuple(
@@ -25,6 +26,7 @@ AmplifierRange = namedtuple(
     ]
 )
 
+# Values found in NetForce software, change based on gain and excitation voltage
 AMPLIFIER_RANGE = AmplifierRange(
     Fx_min=-192.73,
     Fx_max=192.73,
@@ -40,7 +42,8 @@ AMPLIFIER_RANGE = AmplifierRange(
     Mz_max=37.77
 )
 
-SAMPLES_TO_SHOW = 2000
+# Seconds of recorded data to show
+SECONDS_TO_SHOW = 5
 
 
 class PlotWidget(QWidget):
@@ -86,9 +89,6 @@ class PlotWidget(QWidget):
 
         super().__init__(parent=parent)
 
-        # Initiate the pyqygraph widget
-        self.plots = Plots(self)
-
         # Initiate the timer that updates the graphs
         self.timer = QTimer(parent=self)
         self.timer.setInterval(33.33)  # ~30Hz, the faster the more resource intensive the app
@@ -97,15 +97,29 @@ class PlotWidget(QWidget):
         # Initiate variables to store incoming data from DataWorker
         # TODO: Get rid of this magic number and use the sample rate multiplied
         # by however many seconds should be shown
-        self.cop_xdirection = [0 for i in range(SAMPLES_TO_SHOW)]
-        self.cop_ydirection = [0 for i in range(SAMPLES_TO_SHOW)]
-        self.force_zdirection = [0 for i in range(SAMPLES_TO_SHOW)]
-        self.emg_tibialis = [0 for i in range(SAMPLES_TO_SHOW)]
-        self.emg_soleus = [0 for i in range(SAMPLES_TO_SHOW)]
+        self._sample_rate = self._read_settings_file()
+        samples_to_show = SECONDS_TO_SHOW * self._sample_rate
+        self.cop_xdirection = [0 for i in range(samples_to_show)]
+        self.cop_ydirection = [0 for i in range(samples_to_show)]
+        self.force_zdirection = [0 for i in range(samples_to_show)]
+        self.emg_tibialis = [0 for i in range(samples_to_show)]
+        self.emg_soleus = [0 for i in range(samples_to_show)]
+
+        # Initiate the pyqygraph widget
+        self.plots = Plots(self, self._sample_rate)
 
         layout = QVBoxLayout()
         layout.addWidget(self.plots)
         self.setLayout(layout)
+
+    def _read_settings_file(self):
+        """Read the settings file to get the sample rate."""
+        with open("amti_settings.json", 'r') as file:
+            settings = json.load(file)
+
+        sample_rate = settings["sample_rate"]
+
+        return sample_rate
 
     @Slot(np.ndarray)
     def process_data_from_worker(self, data: np.ndarray) -> None:
@@ -162,15 +176,20 @@ class PlotWidget(QWidget):
 class Plots(GraphicsLayoutWidget):
     """A class to display a multi-panel pyqtgraph figure."""
 
-    def __init__(self, parent) -> None:
+    def __init__(self, parent, sample_rate) -> None:
         """
         Parameters
         ----------
         parent : PySide6.QtWidgets.QWidget
             the parent widget
+        sample_rate : int
+            sample rate of the DAQ
         """
 
         super().__init__(parent=parent)
+
+        # Set cutoff for displaying 1 second of data on the CoP graph
+        self._cop_cutoff = (-1) * sample_rate
 
         # Create the center of pressure graph
         self.cop_plot_item = self.addPlot(row=0, col=0, title="Center of Pressure (m)")
@@ -226,8 +245,7 @@ class Plots(GraphicsLayoutWidget):
             emg data from soleus sensor
         """
 
-        # self.cop_plot_line.setData(x=[cop_xdirection[-1]], y=[cop_ydirection[-1]])
-        self.cop_plot_line.setData(x=cop_xdirection, y=cop_ydirection)
+        self.cop_plot_line.setData(x=cop_xdirection[self._cop_cutoff:], y=cop_ydirection[self._cop_cutoff:])
         self.fz_plot_line.setData(y=force_zdirection)
         self.emg_tibialis_plot_line.setData(y=emg_tibialis)
         self.emg_soleus_plot_line.setData(y=emg_soleus)
