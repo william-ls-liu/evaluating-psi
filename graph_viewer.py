f41806a2b1af84e8ce6e83f721bcc61b4903c56e
William Liu <will123liu@gmail.com>
2023-01-23 10:34:19 -0800

    feat: add class to plot step trial
    Create base class `GraphDialog` that `BaselineGraphDialog` and
    `StepGraphDialog` inherit from. Add `StepGraphDialog` class to plot data
    from a step trial.

--- graph_viewer.py
+++ graph_viewer.py
@@ -7,12 +7,23 @@ from matplotlib.figure import Figure
 matplotlib.use('Qt5Agg')
 
 
-class BaselineGraphDialog(QDialog):
-    """This class represents a dialog window that shows a MPL graph."""
-    def __init__(self, data, parent=None):
-        super().__init__(parent)
+# Indexes of platform axes
+FX = 0
+FY = 1
+FZ = 2
+MX = 3
+MY = 4
+MZ = 5
+EMG_1 = 6
+EMG_2 = 7
 
-        self.setWindowTitle("Baseline APA Viewer")
+
+class GraphDialog(QDialog):
+    """Base class for graph windows."""
+
+    def __init__(self, window_title: str, parent=None) -> None:
+        super().__init__(parent)
+        self.setWindowTitle(window_title)
 
         # Define the buttons for the dialog box
         self.button_box = QDialogButtonBox()
@@ -21,24 +32,61 @@ class BaselineGraphDialog(QDialog):
         self.button_box.accepted.connect(self.accept)
         self.button_box.rejected.connect(self.reject)
 
-        # Create the graph
-        self.figure = MplCanvas()
-        self.figure.cop_graph.plot(data)
-        self.figure.cop_graph.set_title("APA")
+        # Create the canvas
+        self.canvas = MplCanvas()
 
         # Add the navigation toolbar
-        self.toolbar = NavigationToolbar2QT(self.figure, self)
+        self.toolbar = NavigationToolbar2QT(self.canvas, self)
 
         layout = QVBoxLayout()
         layout.addWidget(self.toolbar)
-        layout.addWidget(self.figure)
+        layout.addWidget(self.canvas)
         layout.addWidget(self.button_box)
         self.setLayout(layout)
 
 
+class BaselineGraphDialog(GraphDialog):
+    """A dialog window that shows a baseline trial graph."""
+
+    def __init__(self, data, parent=None):
+        super().__init__("Baseline APA Viewer", parent)
+
+        self.mediolateral_force_graph = self.canvas.figure.add_subplot(1, 1, 1)
+        self.mediolateral_force_graph.plot(data)
+        self.mediolateral_force_graph.set_title("Mediolateral Force (N)")
+
+
+class StepGraphDialog(GraphDialog):
+    """A dialog window that shows a step trial graph."""
+
+    def __init__(self, data, parent=None) -> None:
+
+        super().__init__("Step Trial Viewer", parent)
+
+        self.mediolateral_force_graph = self.canvas.figure.add_subplot(3, 2, 1)
+        self.mediolateral_force_graph.plot([row[FX] for row in data])
+        self.mediolateral_force_graph.set_title("Mediolateral Force (N)")
+
+        self.anteroposterior_force_graph = self.canvas.figure.add_subplot(3, 2, 2)
+        self.anteroposterior_force_graph.plot([row[FY] for row in data])
+        self.anteroposterior_force_graph.set_title("Anteroposterior Force (N)")
+
+        self.vertical_force_graph = self.canvas.figure.add_subplot(3, 2, 3)
+        self.vertical_force_graph.plot([row[FZ] for row in data])
+        self.vertical_force_graph.set_title("Vertical Force (N)")
+
+        self.emg_tibialis_graph = self.canvas.figure.add_subplot(3, 2, 4)
+        self.emg_tibialis_graph.plot([row[EMG_1] for row in data])
+        self.emg_tibialis_graph.set_title("EMG Tibialis (V)")
+
+        self.emg_soleus_graph = self.canvas.figure.add_subplot(3, 2, 5)
+        self.emg_soleus_graph.plot([row[EMG_2] for row in data])
+        self.emg_soleus_graph.set_title("EMG Soleus (V)")
+
+
 class MplCanvas(FigureCanvas):
     """The canvas where the graph is drawn."""
+
     def __init__(self, parent=None, width=10, height=10, dpi=200):
         self.figure = Figure(figsize=(width, height), dpi=dpi, layout='tight')
-        self.cop_graph = self.figure.add_subplot(1, 1, 1)
         super().__init__(figure=self.figure)
